c914d6869188b1d46c1ea66c8a8325cfc30c6692
amartynov-pantus <WAcuSUQSe56HsjmZ>
2020-10-28 10:56:25 +0400

    ADM-44 Рефактор имен + вьюкс

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import CatalogBrands from "@/store/module/catalog/Brands";
-import CatalogCategories from "@/store/module/catalog/Categories";
-import CatalogApplicabilities from "@/store/module/catalog/Applicabilities";
-import NewsArticles from "@/store/module/news/Articles"
-import NewsCategory from "@/store/module/news/Category"
-import Authentication from "@/store/module/services/Authentication"
-import ProductParts from "@/store/module/products/Parts";
-import FileManager from "@/store/module/services/File-manager";
+import CatalogBrands from "@/store/module/catalog/brands";
+import CatalogCategories from "@/store/module/catalog/categories";
+import CatalogApplicabilities from "@/store/module/catalog/applicabilities";
+import NewsArticles from "@/store/module/news/articles"
+import NewsCategory from "@/store/module/news/category"
+import Authentication from "@/store/module/services/authentication"
+import ProductParts from "@/store/module/products/parts";
+import FileManager from "@/store/module/services/file-manager";
 
 
 Vue.use(Vuex)
